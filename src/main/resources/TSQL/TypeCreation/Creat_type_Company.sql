a00fa04f3e6b3b30ccdb6350c9bbd42855fe773d
Sven Büchner <sven.buechner.privat@googlemail.com>
2019-05-21 18:03:16 +0200

    Removed Create_Type_Company.sql from project

--- src/main/resources/TSQL/TypeCreation/Creat_type_Company.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TYPE type_Company AS TABLE
-(
-    fld_CompanyID       INT,
-    fld_CvrNr           VARCHAR(8)  NOT NULL,
-    fld_CompanyName     VARCHAR(50) NOT NULL
-    --fld_EducationListID INT
-);
