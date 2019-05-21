6661ac466b5e47c552dcfd7e4972d20076e3595e
MattyxD19 <mathiasbc97@gmail.com>
2019-05-21 12:21:58 +0200

    Added sub menus

--- src/main/java/Dummy.java
+++ src/main/java/Dummy.java
@@ -1,2 +1,11 @@
 public class Dummy {
+
+    public static int addNumber(int a, int b){
+
+
+
+        return a + b;
+
+    }
+
 }
--- src/test/java/DummyTest.java
+++ src/test/java/DummyTest.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DummyTest {
+    @Test
+    public void addNumber(){
+        assertEquals(Dummy.addNumber(2,2),4);
+        assertEquals(Dummy.addNumber(2,2),5);
+
+    }
 }
