91c4dd15eb9e146173e529e93ae6e44f6174e5b3
Sven Büchner <sven.buechner.privat@googlemail.com>
2019-05-22 18:33:13 +0200

    fixed minor typo

--- src/main/resources/TSQL/TableCreation/Create_tbl_Consultation_Employee_Bridge.sql
+++ src/main/resources/TSQL/TableCreation/Create_tbl_Consultation_Employee_Bridge.sql
@@ -1,5 +1,5 @@
 USE db_SmartAcademy;
-CREATE TABLE tbl_Consultation_Employee_bridge
+CREATE TABLE tbl_Consultation_Employee_Bridge
 (
     fld_ConsultationID INT NOT NULL,
     FOREIGN KEY (fld_ConsultationID) REFERENCES tbl_Consultation (fld_ConsultationID) ON DELETE CASCADE,
