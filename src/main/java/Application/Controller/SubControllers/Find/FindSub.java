dfe5800e6935e29e159c527065c44f928cd87dfc
Sven Büchner <sven.buechner.privat@googlemail.com>
2019-05-31 21:48:37 +0200

    impleneted reset for FindSub

--- src/main/java/Application/Controller/SubControllers/Find/FindSub.java
+++ src/main/java/Application/Controller/SubControllers/Find/FindSub.java
@@ -1,144 +1,139 @@
 package Application.Controller.SubControllers.Find;
 
 import Application.Controller.AbstractController;
+import Application.SearchContainer;
+import Domain.Company;
+import Domain.Consultation;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
 
-public class FindSub extends AbstractController {
-    public TitledPane companyPane;
-    public TextField companyIDTextField;
-    public Tooltip companyIDTooltip;
-    public TextField cvrNrTextField;
-    public Tooltip cvrNrTooltip;
-    public TextField companyNameTextField;
-    public Tooltip companyNameTooltip;
-    public TitledPane consultationPane;
-    public DatePicker consultationMaxDatePicker;
-    public Tooltip consultationMaxDateTooltip;
-    public DatePicker consultationMinDatePicker;
-    public Tooltip consultationMinDateTooltip;
-    public TextField consultationNameTextField;
-    public Tooltip consultationNameTooltip;
-    public TextField consultationIDTextField;
-    public Tooltip consultationIDTooltip;
-    public TitledPane employeePane;
-    public TextField employeeIDTextField;
-    public Tooltip employeeIDTooltip;
-    public TextField empFirstNameTextField;
-    public Tooltip empFirstNameTooltip;
-    public TextField empLastNameTextField;
-    public Tooltip empLastNameTooltip;
-    public TextField cprNrTextField;
-    public Tooltip cprNrTooltip;
-    public TextField eMailTextField;
-    public Tooltip eMailTooltip;
-    public TextField phoneNrTextField;
-    public Tooltip phoneNrTooltip;
-    public TitledPane interviewPane;
-    public TextField interviewIDTextField;
-    public Tooltip interviewIDTooltip;
-    public TextField interviewsNameTextField;
-    public Tooltip interviewNameTooltip;
-    public TitledPane educationPane;
-    public TextField amuNrTextField;
-    public Tooltip amuNrTooltip;
-    public TextField educationNameTextField;
-    public Tooltip educationNameTooltip;
-    public TextField educationNoOfDaysTextField;
-    public Tooltip educationNoOfDaysTooltip;
-    public DatePicker educationMinDatePicker;
-    public Tooltip educationMinDateTooltip;
-    public DatePicker educationMaxDatePicker;
-    public Tooltip educationMaxDateTooltip;
-    public TitledPane providerPane;
-    public TextField providerIDTextField;
-    public Tooltip providerIDTooltip;
-    public TextField providerNameTextField;
-    public Tooltip providerNameTooltip;
+import java.util.ArrayList;
 
-    public SimpleBooleanProperty isValid; // Hook for parent class to activate confirm button
+public class FindSub extends AbstractController { // FIXME: 31/05/2019 at some point this class neec to be claned up, we realy dont validate input we do that at the SearchContainer
+
+    //Company
+    public TextField companyIDTextField;
+    public TextField cvrNrTextField;
+    public TextField companyNameTextField;
+    //Consultation
+    public DatePicker consultationMaxDatePicker;
+    public DatePicker consultationMinDatePicker;
+    public TextField consultationNameTextField;
+    public TextField consultationIDTextField;
+    //Employee
+    public TextField employeeIDTextField;
+    public TextField empFirstNameTextField;
+    public TextField empLastNameTextField;
+    public TextField cprNrTextField;
+    public TextField eMailTextField;
+    public TextField phoneNrTextField;
+    //Interview
+    public TextField interviewIDTextField;
+    public TextField interviewsNameTextField;
+    //Education
+    public TextField amuNrTextField;
+    public TextField educationNameTextField;
+    public TextField educationNoOfDaysTextField;
+    public DatePicker educationMinDatePicker;
+    public DatePicker educationMaxDatePicker;
+    //provider
+    public TextField providerIDTextField;
+    public TextField providerNameTextField;
+
     public Button searchButton;
     public Button resetButton;
 
+    private ArrayList<Company> searchResult;
+    private SearchContainer previousSearchContainer = null;
 
-    public void initialize(){
-        //setup isValid
-        //hookup bindings
+
+    public void initialize() {
+        //Load previous search? //TODO consider this functionality
+        resetToEmpty();
     }
 
-    public void handleCompanyIDInput(KeyEvent keyEvent) {
-    }
-
-    public void handleCvrNrInput(KeyEvent keyEvent) {
-    }
-
-    public void handleCompanyNameInput(KeyEvent keyEvent) {
-    }
-
-    public void handleConsulationDatePicked(ActionEvent event) {
-    }
-
-    public void handleConsultationNameInput(KeyEvent keyEvent) {
-    }
-
-    public void handleConsultationIDInput(KeyEvent keyEvent) {
-    }
-
-    public void handleEmployeeIDInput(KeyEvent keyEvent) {
-    }
-
-    public void handleEmpFirstNameInput(KeyEvent keyEvent) {
-    }
-
-    public void handleEmpLastNameInput(KeyEvent keyEvent) {
-    }
-
-    public void handleCprNrInput(KeyEvent keyEvent) {
-    }
-
-    public void handleEmailInput(KeyEvent keyEvent) {
-    }
-
-    public void handlePhoneNrInput(KeyEvent keyEvent) {
-    }
-
-    public void handleInterviewIDInput(KeyEvent keyEvent) {
-    }
-
-    public void handleInterviewNameInput(KeyEvent keyEvent) {
-    }
-
-    public void handleAmuNrInput(KeyEvent keyEvent) {
-    }
-
-    public void handleEducationNameInput(KeyEvent keyEvent) {
-    }
-
-    public void handleEducationNoOfDaysInput(KeyEvent keyEvent) {
-    }
-
-    public void handleEducationDatePicked(ActionEvent event) {
-    }
-
-    public void handleProviderIDInput(KeyEvent keyEvent) {
-    }
-
-    public void handleProviderNameInput(KeyEvent keyEvent) {
-    }
-
-    public void updateIsValid(){
-        // Manages the isValid property aka when all values are valid = true
-    }
-
-    public void resetForm(){
-        //Reset fields, set field if it has a selected Domain object
+    @Override
+    public void initValues(SearchContainer searchContainer) {
+        this.previousSearchContainer = searchContainer;
+        resetToPreviousSearch();
     }
 
     public void handleSearch(ActionEvent event) {
+        //TODO need to fix DbFacde first
     }
 
+    /**
+     * Reset all field to be empty or if a previous SearchContainer was loaded set the field to the previous values.
+     * @param event user clicked the reset button.
+     */
+    @SuppressWarnings("Duplicates")
     public void handleReset(ActionEvent event) {
+        if (previousSearchContainer == null) {
+            resetToEmpty();
+        } else {
+            resetToPreviousSearch();
+        }
+    }
+
+    private void resetToEmpty(){
+        companyIDTextField.setText("");
+        cvrNrTextField.setText("");
+        companyNameTextField.setText("");
+        //Consultation
+        consultationMaxDatePicker.getEditor().setText(null);
+        consultationMinDatePicker.getEditor().setText(null);
+        consultationNameTextField.setText("");
+        consultationIDTextField.setText("");
+        //Employee
+        employeeIDTextField.setText("");
+        empFirstNameTextField.setText("");
+        empLastNameTextField.setText("");
+        cprNrTextField.setText("");
+        eMailTextField.setText("");
+        phoneNrTextField.setText("");
+        //Interview
+        interviewIDTextField.setText("");
+        interviewsNameTextField.setText("");
+        //Education
+        amuNrTextField.setText("");
+        educationNameTextField.setText("");
+        educationNoOfDaysTextField.setText("");
+        educationMinDatePicker.getEditor().setText(null);
+        educationMaxDatePicker.getEditor().setText(null);
+        //provider
+        providerIDTextField.setText("");
+        providerNameTextField.setText("");
+    }
+
+    private void resetToPreviousSearch(){
+        companyIDTextField.setText(previousSearchContainer.getCompanyID().toString());
+        cvrNrTextField.setText(previousSearchContainer.getCompanyName());
+        companyNameTextField.setText(previousSearchContainer.getCompanyName());
+        //Consultation
+        consultationMaxDatePicker.getEditor().setText(previousSearchContainer.getConsultationMaxDate().toString());
+        consultationMinDatePicker.getEditor().setText(previousSearchContainer.getConsultationMinDate().toString());
+        consultationNameTextField.setText(previousSearchContainer.getConsultationName());
+        consultationIDTextField.setText(previousSearchContainer.getConsultationID().toString());
+        //Employee
+        employeeIDTextField.setText(previousSearchContainer.getEmployeeID().toString());
+        empFirstNameTextField.setText(previousSearchContainer.getEmployeeFirstName());
+        empLastNameTextField.setText(previousSearchContainer.getEmployeeLastName());
+        cprNrTextField.setText(previousSearchContainer.getCprNr());
+        eMailTextField.setText(previousSearchContainer.getEmail());
+        phoneNrTextField.setText(previousSearchContainer.getPhoneNr());
+        //Interview
+        interviewIDTextField.setText(previousSearchContainer.getInterviewID().toString());
+        interviewsNameTextField.setText(previousSearchContainer.getInterviewName());
+        //Education
+        amuNrTextField.setText(previousSearchContainer.getAmuNr().toString());
+        educationNameTextField.setText(previousSearchContainer.getEducationName());
+        educationNoOfDaysTextField.setText(previousSearchContainer.getEducationNoOfDays().toString());
+        educationMinDatePicker.getEditor().setText(previousSearchContainer.getEducationMinDate().toString());
+        educationMaxDatePicker.getEditor().setText(previousSearchContainer.getEducationMaxDate().toString());
+        //provider
+        providerIDTextField.setText(previousSearchContainer.getProviderID().toString());
+        providerNameTextField.setText(previousSearchContainer.getProviderName());
     }
 }
