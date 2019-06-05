12a68d1a9ba54e90251718f91001704fa98406e5
Sven Büchner <sven.buechner.privat@googlemail.com>
2019-06-05 09:23:44 +0200

    implemented FindEducationToChange

--- src/main/java/Application/Controller/Find/FindToChange/FindEducationToChange.java
+++ src/main/java/Application/Controller/Find/FindToChange/FindEducationToChange.java
@@ -2,10 +2,15 @@ package Application.Controller.Find.FindToChange;
 
 import Application.Controller.AbstractController;
 import Application.Controller.SubControllers.Find.FindEducationSub;
+import Application.Controller.ViewController;
 import Application.SearchContainer;
+import Domain.Education;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
 
 public class FindEducationToChange extends AbstractController {
 
@@ -14,23 +19,48 @@ public class FindEducationToChange extends AbstractController {
     @FXML
     private Button confirmationButton; //Button needs to be disable when form is not correct
 
+    private TableView<Education> educationTableView;
+
+
     @FXML
     private void initialize(){
-        // hook up the  button with subcontroller form correctness
+        // Load the TableView reference from subController
+        educationTableView = findEducationSubController.getEducationTableView();
+
+        // hook up the confirmation button
+        confirmationButton.disableProperty().bind(educationTableView.getSelectionModel().selectedItemProperty().isNull());
     }
 
+    /**
+     * {@inheritDoc}
+     * <br/><br/>
+     * Calling this initValues will ultimately invoke {@link Application.Controller.SubControllers.Find.FindSub#initValues(SearchContainer) FindSub} reset.
+     * This will set the search form to the values of the given SearchContainer.
+     */
     @Override
     public void initValues(SearchContainer searchContainer) {
         //Send to FindSub controller to fill out form and reset
+        findEducationSubController.initValues(searchContainer);
     }
 
     @FXML
     private void handleCancel(ActionEvent event) {
         //Return to main screen
+        Parent root = confirmationButton.getScene().getRoot();
+        root.getScene().setRoot(ViewController.MAIN_CONTROLLER.loadParent());
     }
 
     @FXML
     private void handleConfirmation(ActionEvent event) {
-        //goto next
+        //Get selection
+        Education toBeChangedCompany = educationTableView.getSelectionModel().getSelectedItem();
+
+        //Get the search container
+        SearchContainer currentSearch = findEducationSubController.getFindSubController().getCurrentSearchContainer();
+
+        //Goto Change Company
+        Parent root = confirmationButton.getScene().getRoot();
+        ((BorderPane) root).setCenter(ViewController.ALTER_COMPANY.loadParent(currentSearch, toBeChangedCompany));
+
     }
 }
